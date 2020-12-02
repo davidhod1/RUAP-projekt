a59d4b2a1fbe6bc378e363b57304324c3316729f
davidhod1 <60391361+davidhod1@users.noreply.github.com>
2020-12-02 18:52:05 +0100

    Testing app

---
+++ RUAP_test/RUAP_test/RUAP_test.cpp
@@ -0,0 +1,10 @@
+// RUAP_test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Pozdrav iz aplikacije od Davida Hodaka\n";
+}
+
